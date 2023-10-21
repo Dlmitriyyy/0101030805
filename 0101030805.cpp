7ed0e1258e42d5b74b50c18b82a19693cdc7f11a
Dlmitriyyy <streltsowdm@gmail.com>
2023-10-21 20:03:36 +0300

    Добавьте файлы проекта.

---
+++ 0101030805.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+#include <array>
+#include <time.h>
+using namespace std;
+int main()
+{
+	system("chcp 1251 > nul");
+	array <int, 10> mass;
+	int max_number, min_number;
+	srand(time(NULL));
+	cout << "Сгенерований масив чисел: ";
+	for (int a = 0; a < mass.size(); a++)
+	{
+		mass[a] = rand() % 20;
+		if (a == 0)
+		{
+			max_number = mass[a];
+			min_number = mass[a];
+		}
+		if (mass[a] > max_number)
+		{
+			max_number = mass[a];
+		}
+		if (mass[a] < min_number)
+		{
+			min_number = mass[a];
+		}
+		cout << mass[a] << " ";
+	}
+	cout << endl;
+	cout << "Максимальне число масиву: " << max_number << endl << "Мінімальне число масиву: " << min_number;
+	
+}
